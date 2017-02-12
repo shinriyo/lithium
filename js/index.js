78ec8c850510d3603445b7269fc6e57a20156bcb
shinriyo <shinriyo@shinriyos-MacBook.local>
2017-02-12 21:11:48 +0900

    indicator

--- js/index.js
+++ js/index.js
@@ -1,19 +1,17 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
-require('crash-reporter').start();
-var mainWindow = null;
-app.on('window-all-closed', function() {
-    //if (process.platform != 'darwin')
-    app.quit();
-});
-app.on('ready', function() {
-    // ブラウザ(Chromium)の起動, 初期画面のロード
-    mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 600
-    });
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
-});
+    "use strict;"
+
+    onload = () => {
+        const webview = document.getElementById("webview_wrapper");
+        const indicator = document.querySelector('.indicator')
+
+        const loadstart = () => {
+            indicator.innerText = 'loading...'
+        }
+
+        const loadstop = () => {
+            indicator.innerText = ''
+        }
+
+        webview.addEventListener('did-start-loading', loadstart)
+        webview.addEventListener('did-stop-loading', loadstop);
+    }
