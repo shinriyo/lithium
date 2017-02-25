c4a968533ef46293f2ae3686245dfd42ffd0bb37
shinriyo <shinriyo@shinriyos-MacBook.local>
2017-02-25 10:29:18 +0900

    cleanup

--- js/index.js
+++ js/index.js
@@ -1,26 +1,17 @@
-    "use strict;"
+"use strict;"
 
-    onload = () => {
-        const webview = document.getElementById("webview_wrapper");
-        const indicator = document.querySelector('.indicator')
+onload = () => {
+    const webview = document.getElementById("webview_wrapper");
+    const indicator = document.querySelector('.indicator')
 
-        const loadstart = () => {
-            indicator.innerText = 'loading...'
-        }
-
-        const loadstop = () => {
-            indicator.innerText = ''
-        }
-
-        webview.addEventListener('did-start-loading', loadstart)
-        webview.addEventListener('did-stop-loading', loadstop);
-
-        var checkUnreadCountTimer = setInterval(function() {
-            webview.send("window-data");
-        }, 1000);
-
-
-        const { ipcRenderer } = require('electron');
-        window.addEventListener('load', () => ipcRenderer.send('ipc', 'webview.htmlのpreloadのwindow.onload'));
-        ipcRenderer.send('ipc', 'webview.htmlのpreload');
+    const loadstart = () => {
+        indicator.innerText = 'loading...'
     }
+
+    const loadstop = () => {
+        indicator.innerText = ''
+    }
+
+    webview.addEventListener('did-start-loading', loadstart)
+    webview.addEventListener('did-stop-loading', loadstop);
+}
